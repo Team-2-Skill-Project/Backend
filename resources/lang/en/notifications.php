6a3e658475ca63729a8021b8aefbe2040e7d16ce
Ahmed Maarouf <ahmedwael5775@gmail.com>
2026-09-15 11:14:57 +0300

    feat: add localized notification hooks

---
+++ resources/lang/en/notifications.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'relevant_job' => [
+        'title' => 'New relevant job',
+        'message' => 'A new job matching your profile is available.',
+    ],
+    'roadmap_reminder' => [
+        'title' => 'Roadmap reminder',
+        'message' => 'You have a roadmap step waiting for you.',
+    ],
+];
